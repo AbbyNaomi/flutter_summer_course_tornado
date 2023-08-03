d501fe15f5230b1ce22860c23193b4ffd961dd2a
Tornado-12 <tornado-12@Admin-12s-iMac.local>
2023-08-03 18:21:46 +0800

    add:wordfind 17

--- wordfind_app/lib/main.dart
+++ wordfind_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:wordfind_app/start_page.dart';
 import 'package:wordfind_app/task_page.dart';
 import 'package:wordfind_app/welcome_page.dart';
 
+
 void main() {
   runApp(MaterialApp(
     title: 'Word Find Game',
--- wordfind_app/lib/task_page.dart
+++ wordfind_app/lib/task_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'char_model.dart';
+import 'task_model.dart';
+import 'package:word_search_safety/word_search_safety.dart';
 
 class TaskPage extends StatelessWidget {
   const TaskPage({super.key});
@@ -57,6 +62,172 @@ class TaskPage extends StatelessWidget {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class TaskWidget extends StatefulWidget {
+  const TaskWidget(this.size, this.listQuestions, {super.key});
+  final Size size;
+  final List<TaskModel> listQuestions;
+
+  @override
+  State<TaskWidget> createState() => _TaskWidgetState();
+}
+
+class _TaskWidgetState extends State<TaskWidget> {
+  late Size size;
+  late List<TaskModel> listQuestions;
+  int currentQuestionIndex = 0;
+  int hintCount = 0;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    size = widget.size;
+    listQuestions = widget.listQuestions;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    TaskModel currentQuestion = listQuestions[currentQuestionIndex];
+    return SizedBox(
+      width: double.maxFinite,
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                InkWell(
+                  onTap: generateHint(),
+                  child: Row(
+                    children: [
+                      InkWell(
+                        onTap: () => generatePuzzle(left: true),
+                      ),
+                      InkWell(
+                        child: Icon(
+                          Icons.arrow_back_ios,
+                          size: 45,
+                          color: Color(0xFFE86B02),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: generatePuzzle(next: true),
+                        child: Icon(
+                          Icons.arrow_forward_ios,
+                          size: 45,
+                          color: Color(0xFFE86B02),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+              child: Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(10),
+            child: Container(
+              alignment: Alignment.center,
+              constraints: BoxConstraints(maxWidth: size.width / 2 * 1.5),
+              child: Image.network(
+                currentQuestion.pathImage,
+                fit: BoxFit.contain,
+              ),
+            ),
+          )),
+          Container(
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.center,
+            child: Text(
+              currentQuestion.question,
+              style: TextStyle(
+                  fontSize: 25,
+                  color: Color(0xFFE86B02),
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 30),
+            alignment: Alignment.center,
+            child: LayoutBuilder(
+              builder: (context, constrains) {
+                return Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: currentQuestion.puzzles.map((puzzle) {
+                      Color? color;
+                      if (currentQuestion.isDone) {
+                        color = Colors.green[300];
+                      } else if (puzzle.hintShow = true) {
+                        color = Colors.yellow[300];
+                      } else if (currentQuestion.isFull) {
+                        color = Colors.red;
+                      } else {
+                        color = Color(0xFFE86B02);
+                      }
+                      return InkWell(
+                        onTap: () {
+                          if (puzzle.hintShow || currentQuestion.isDone) return;
+                          currentQuestion.isFull = false;
+                          puzzle.clearValue();
+                          setState(() {});
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                              color: color,
+                              borderRadius: BorderRadius.circular(10)),
+                          width: constrains.biggest.width / 7 - 6,
+                          height: constrains.biggest.height / 7 - 6,
+                          margin: EdgeInsets.all(3),
+                          child: Text(
+                            (puzzle.currentValue ?? '').toUpperCase(),
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      );
+                    }).toList());
+              },
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30))),
+            padding: EdgeInsets.only(left: 30, right: 30, top: 30, bottom: 40),
+            alignment: Alignment.center,
+            child: Row(
+              children: [
+                GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      childAspectRatio: 1,
+                      crossAxisCount: 7,
+                      crossAxisSpacing: 4,
+                      mainAxisSpacing: 4,
+                    ),
+                    itemCount: 14,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      bool statusBtn = currentQuestion.puzzles.indexWhere(
+                              (puzzle) => puzzle.currentIndex == index) >=
+                          0;
+                      return LayoutBuilder(builder: (context, constraints));
+                    }),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
